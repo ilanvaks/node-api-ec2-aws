2190065f897dbe9153c334edf4f7b3d5bbf4ffdf
IlanVaks <ilanvaks@gmail.com>
2023-05-04 15:59:22 -0400

    added-dogs

--- index.js
+++ index.js
@@ -6,6 +6,34 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+const dogs = [ 
+  {
+  name: "Paco",
+  breed: "Chihauhua",
+  age: 34
+  },
+  {
+  name: "Paco",
+  breed: "Chihauhua",
+  age: 34
+  },
+  {
+  name: "Paco",
+  breed: "Labrador",
+  age: 34
+  },
+  {
+  name: "Paco",
+  breed: "Yorki",
+  age: 34
+  },
+  {
+  name: "Bob",
+  breed: "Pitbull",
+  age: 34
+ }
+]
+
 
 app.get("/", (req, res) => res.send("Welcome to my api"))
 
